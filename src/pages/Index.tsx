8b5f7d2cc14d5937ae7289804127b14390dbf9f1
pnb-rae <ryanwaweru475@gmail.com>
2025-12-14 11:52:21 +0300

    new update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,6 @@ import Header from "@/components/Header"
 import Hero from "@/components/Hero"
 import About from "@/components/About"
 import Services from "@/components/Services"
-import Skills from "@/components/Skills"
 import Statistics from "@/components/Statistics"
 import Projects from "@/components/Projects"
 import Testimonials from "@/components/Testimonials"
@@ -22,7 +21,6 @@ const Index = () => {
           <Hero />
           <About />
           <Services />
-          <Skills />
           <Statistics />
           <Projects />
           <Testimonials />
